8567b1c69f62f9733a98216b69dc2224ce20c896
saito gen <gensaito921@gmail.com>
2023-05-08 16:35:56 +0900

    second

---
+++ 0508/cookie/cookie1.php
@@ -0,0 +1,24 @@
+<?php
+    if(isset($_COOKIE["count"])){
+        $count=$_COOKIE["count"]+1;
+    }
+    else{
+        $count=1;
+    }
+    $flg=setcookie("count",$count,time()+5);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>クッキー</title>
+</head>
+<body>
+    <?php
+    print "<p>訪問回数 : ".$count."回</p>"
+    ?>
+    
+</body>
+</html>
